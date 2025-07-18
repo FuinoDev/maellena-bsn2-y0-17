727fcdd5d8a1a6c80ae3e422e1af4a94b9136dfc
Joshua Andrew Aboga <j.andrewaboga@gmail.com>
2025-07-19 00:17:44 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-const images = ["img/hero1.avif", "img/hero2.avif", "img/hero3.webp"];
+const images = ["img/dt5.jpg", "img/dt3.jpg", "img/dt7.jpg", 
+  "img/dt1.jpg", "img/dt2.jpg", "img/dt4.jpg", "img/dt6.jpg", "img/dt5.jpg"];
 let current = 0;
 const imgElement = document.getElementById("slider-img");
 
@@ -15,6 +16,14 @@ function prevImage() {
   current = (current - 1 + images.length) % images.length;
   showImage(current);
 }
+const navLinks = document.querySelectorAll('.nav-links li a');
+
+navLinks.forEach(link => {
+  link.addEventListener('click', function() {
+    navLinks.forEach(link => link.classList.remove('active')); // Remove from all
+    this.classList.add('active'); // Add to clicked
+  });
+});
 
 // Auto-slide every 3 seconds
-setInterval(nextImage, 3000);
+setInterval(nextImage, 2000);
